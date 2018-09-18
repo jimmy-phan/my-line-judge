6efc7163ebbeceb7a321affad032aff5700224be
Jimmy <jpphan93@yahoo.com>
2018-09-17 20:52:09 -0600

    Fixed webcam_record to stream the video and press a button to record. Modified webcam_play to handle video input and to exit without errors.
    Signed-off-by: Jimmy <jpphan93@yahoo.com>

--- webcam_play.py
+++ webcam_play.py
@@ -1,20 +1,27 @@
 import imutils
 import cv2
+import os
+
+# in_loc = os.path.join("videos", "test8.avi")
+# cap = cv2.VideoCapture(in_loc)
+
 cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
 cap.set(cv2.CAP_PROP_FPS, 120)
 
-while(True):
+while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
-    frame = imutils.resize(frame, width=640)
-    # frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    if frame is not None:
+        frame = imutils.resize(frame, width=640)
+        # frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
-
 
 # When everything done, release the capture
 cap.release()
--- webcam_record.py
+++ webcam_record.py
@@ -2,30 +2,69 @@ import numpy as np
 import imutils
 import cv2
 
-video = cv2.VideoCapture(1)
-video.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
-video.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
-video.set(cv2.CAP_PROP_FPS, 120)
+cap = cv2.VideoCapture(1)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+cap.set(cv2.CAP_PROP_FPS, 120)
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('test8.avi', fourcc, 120.0, (640, 480))
+out = cv2.VideoWriter('test8.avi', fourcc, (cap.get(cv2.CAP_PROP_FPS)), (640, 480))
 
-while video.isOpened():
-    ret, frame = video.read()
-    if ret:
-        # frame = cv2.flip(frame, 0)
+while True:
+    ret, frame = cap.read()
 
-        # # write the flipped frame
-        out.write(frame)
+    k = cv2.waitKey(1)
+    if k % 256 == 32:
+        print("Recording...")
+        while True:
+            ret, frame = cap.read()
+            out.write(frame)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("Recording stopped...")
+                break
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Exiting...")
         break
 
+    # k = cv2.waitKey(1)
+    # if k % 256 == 32:
+    #     print("Recording...")
+    #     if ret:
+    #         qqqq# frame = cv2.flip(frame, 0)
+    #
+    #         # # write the flipped frame
+    #         out.write(frame)
+    #
+    #         cv2.imshow('frame', frame)
+    #         if cv2.waitKey(1) & 0xFF == ord('q'):
+    #             break
+
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     print("q pressed Exit camera")
+    #     cap.release()
+    #     cv2.destroyAllWindows()
+    #     print("Camera closed")
+    #     break
+
+    # if ret:
+    #     # frame = cv2.flip(frame, 0)
+    #
+    #     # # write the flipped frame
+    #     out.write(frame)
+    #
+    #     cv2.imshow('frame', frame)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    #
+    # else:
+    #     break
+
+
 # Release everything if job is finished
-video.release()
+cap.release()
 out.release()
 cv2.destroyAllWindows()
